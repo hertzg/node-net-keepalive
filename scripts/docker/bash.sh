e07342f60cbb2e7f1a86098c56711de352f544f5
George Kotchlamazashvili <hertzg@users.noreply.github.com>
2020-05-23 01:21:24 +0200

    LTS tests, JSDoc3 & Prettier (#17)
    * chore: LTS tests, JSDoc3 & Prettier
    * docker: dev env via docker
    * package: update dependencies
    * github workflow to publish to pages

---
+++ scripts/docker/bash.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+env
+node --version
+npm --version
+
+npm config set loglevel http
+npm config set registry http://verdaccio:4873/
+
+if [[ $NODE_VERSION == 4.* || $NODE_VERSION == 6.* ]];
+then
+  rm -rf node_modules/*
+  npm i --package-lock-only --no-audit
+else
+  npm ci --package-lock-only --no-audit
+fi;
+
+npm test -- --reporter tap
--- test/test-commons.js
+++ test/test-commons.js
@@ -1,82 +1,91 @@
-"use strict"
+'use strict'
 
-const Mockery = require('mockery')
-  , Should = require('should')
-  , Sinon = require('sinon')
+const Mockery = require('mockery'),
+  Should = require('should'),
+  Sinon = require('sinon')
 
 describe('commons', () => {
-
   let sandbox
   beforeEach(() => {
-        Mockery.enable({
-            warnOnReplace: true,
-            warnOnUnregistered: true,
-            useCleanCache: true
-        })
-        Mockery.registerAllowable('../lib/commons')
-        sandbox = Sinon.createSandbox()
+    Mockery.enable({
+      warnOnReplace: true,
+      warnOnUnregistered: true,
+      useCleanCache: true,
     })
+    Mockery.registerAllowable('../lib/commons')
+    sandbox = Sinon.createSandbox()
+  })
 
   afterEach(() => {
-        sandbox.restore()
-        Mockery.deregisterAllowable('../lib/commons')
-        Mockery.disable()
-    })
+    sandbox.restore()
+    Mockery.deregisterAllowable('../lib/commons')
+    Mockery.disable()
+  })
 
   it('should export errnoException function and return exception', () => {
-        Mockery.registerAllowable('util')
-        require('../lib/commons').should.have.property('errnoException')
-            .which.is.a.Function()
-        Mockery.deregisterAllowable('util')
-    })
+    Mockery.registerAllowable('util')
+    require('../lib/commons')
+      .should.have.property('errnoException')
+      .which.is.a.Function()
+    Mockery.deregisterAllowable('util')
+  })
 
   it('should return errnoException style Error', () => {
     Mockery.registerAllowable('util')
-        require('../lib/commons').errnoException(999, 'test_syscall_name', 'original')
-            .should.be.instanceOf(Error)
-            .and.have.properties(['message', 'code', 'errno', 'syscall'])
-            .and.have.properties({
-                syscall: 'test_syscall_name'
-            })
-        Mockery.deregisterAllowable('util')
-    })
+    require('../lib/commons')
+      .errnoException(999, 'test_syscall_name', 'original')
+      .should.be.instanceOf(Error)
+      .and.have.properties(['message', 'code', 'errno', 'syscall'])
+      .and.have.properties({
+        syscall: 'test_syscall_name',
+      })
+    Mockery.deregisterAllowable('util')
+  })
 
   it('should use util._errnoException when available', () => {
-
     const _errnoExceptionStub = sandbox.stub()
 
-
-        Mockery.registerMock('util', {
-            _errnoException: _errnoExceptionStub
-        })
-
-        require('../lib/commons').errnoException(999, 'TEST_SYSCALL_NAME', 'original')
-
-        Mockery.deregisterMock('util')
-
-    sandbox.assert.calledOnce(_errnoExceptionStub)
-        sandbox.assert.calledWith(_errnoExceptionStub, -999, 'TEST_SYSCALL_NAME', 'original')
+    Mockery.registerMock('util', {
+      _errnoException: _errnoExceptionStub,
     })
 
-  it('should try use libuv.errname otherwise', () => {
+    require('../lib/commons').errnoException(
+      999,
+      'TEST_SYSCALL_NAME',
+      'original'
+    )
 
-        Mockery.registerMock('util', {})
+    Mockery.deregisterMock('util')
+
+    sandbox.assert.calledOnce(_errnoExceptionStub)
+    sandbox.assert.calledWith(
+      _errnoExceptionStub,
+      -999,
+      'TEST_SYSCALL_NAME',
+      'original'
+    )
+  })
+
+  it('should try use libuv.errname otherwise', () => {
+    Mockery.registerMock('util', {})
 
     sandbox.stub(process, 'binding')
     const uvErrnameStub = Sinon.stub()
 
-        process.binding.withArgs('uv')
-            .returns({errname: uvErrnameStub})
+    process.binding.withArgs('uv').returns({ errname: uvErrnameStub })
 
-    require('../lib/commons').errnoException(999, 'TEST_SYSCALL_NAME', 'original')
+    require('../lib/commons').errnoException(
+      999,
+      'TEST_SYSCALL_NAME',
+      'original'
+    )
 
     sandbox.assert.calledOnce(process.binding)
-        sandbox.assert.calledWith(process.binding, 'uv')
+    sandbox.assert.calledWith(process.binding, 'uv')
 
     Sinon.assert.calledOnce(uvErrnameStub)
-        Sinon.assert.calledWith(uvErrnameStub, -999)
+    Sinon.assert.calledWith(uvErrnameStub, -999)
 
     Mockery.deregisterMock('util')
-    })
-
+  })
 })
