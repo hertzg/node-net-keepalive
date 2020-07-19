9f0f0122dfca8887af4c59eed1df19930a160750
hertz <georgedot@gmail.com>
2020-07-19 22:46:23 +0400

    ci(release): tweak release configurations

--- release.config.js
+++ release.config.js
@@ -7,28 +7,50 @@ module.exports = {
   preset: 'conventionalcommits',
   releaseRules: [
     { breaking: true, release: 'major' },
+    { type: 'rel', scope: 'major', release: 'major' },
     { type: 'feat', release: 'minor' },
+    { type: 'rel', scope: 'minor', release: 'minor' },
     { type: 'fix', release: 'patch' },
+    { type: 'perf', release: 'patch' },
+    { type: 'deps', release: 'patch' },
+    { scope: 'README', release: 'patch' },
+    { type: 'rel', scope: 'patch', release: 'patch' },
+    { scope: 'no-release', release: false },
   ],
-  changelogFile: 'CHANGELOG.md',
-  changelogTitle: CHANGELOG_HEADER,
-  tarballDir: 'pack',
-  message:
-    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-  assets: ['CHANGELOG.md', 'pack/*.tgz'],
-  assignees: ['hertzg'],
+
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      { changelogFile: 'CHANGELOG.md', changelogTitle: CHANGELOG_HEADER },
+    ],
     [
       '@semantic-release/exec',
       {
         prepareCmd: 'prettier --write CHANGELOG.md',
       },
     ],
-    '@semantic-release/npm',
-    // '@semantic-release/git',
-    '@semantic-release/github',
+    ['@semantic-release/npm', { tarballDir: 'pack' }],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+          'package-lock.json',
+          'npm-shrinkwrap.json',
+        ],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: ['CHANGELOG.md', 'pack/*.tgz'],
+        assignees: ['hertzg'],
+      },
+    ],
   ],
 }
