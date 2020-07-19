cbdf4532fe12100325b78cbc13bd4d38cc17756e
hertz <georgedot@gmail.com>
2020-07-19 18:14:54 +0400

    test(dts): add tests for ts definition

---
+++ index.test-d.ts
@@ -0,0 +1,12 @@
+import * as NetKeepAlive from './'
+import * as Net from 'net'
+import { expectType } from 'tsd'
+
+Net.createServer((incomingSocket) => {
+  expectType<boolean>(NetKeepAlive.setKeepAliveInterval(incomingSocket, 1000))
+  expectType<boolean>(NetKeepAlive.setKeepAliveProbes(incomingSocket, 1))
+})
+
+const clientSocket = Net.createConnection({port: -1})
+expectType<boolean>(NetKeepAlive.setKeepAliveInterval(clientSocket, 1000))
+expectType<boolean>(NetKeepAlive.setKeepAliveProbes(clientSocket, 1))
